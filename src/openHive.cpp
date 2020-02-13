e26e4144ad1bed965956e1e5c5f54c33808d3535
henrikkol <kolberghenrik@gmail.com>
2020-02-13 21:00:36 +0100

    Added the HX711 scale module and reading of wight + removed wifi setup with mqtt

--- src/openHive.cpp
+++ src/openHive.cpp
@@ -1,91 +1,87 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <HX711.h>
+#define TINY_GSM_MODEM_SIM800 // Modem is SIM800
+//#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
 
-
-//Set your own wifi + mqtt broker here
-const char* ssid = "my_ssid";
-const char* password = "password";
-const char* mqtt_server = "server_ip_adress";
-const int mqtt_port = 1234;
-const char* mqtt_user = "mqtt_user";
-const char* mqtt_password = "mqtt_password";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+#define LOADCELL_DOUT_PIN 2
+#define LOADCELL_SCK_PIN 3
 
 #define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP 60          /* Time ESP32 will go to sleep (in seconds) */
 
+#include <Arduino.h>
+#include <HX711.h>
+#include <TinyGsmClient.h>
+
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCELL_DIVIDER = 5895655;
+
+HX711 scale;
+
 RTC_DATA_ATTR int bootCount = 0;
 RTC_DATA_ATTR int stepCount = 0;
-RTC_DATA_ATTR int test[] = {};
+RTC_DATA_ATTR int hiveWeight[] = {};
 
 void sendData()
 {
-  Serial.println("sending data to user");
-  Serial.println("------------------------------------------------------------------------------------");
-  Serial.println("------------------------------------------------------------------------------------");
-  Serial.println("------------------------------------------------------------------------------------");
-  Serial.println("------------------------------------------------------------------------------------");
-
-  for (int i = 0; i <= stepCount; i++)
-  {
-    Serial.println("Random number: " + String(i) + " with value: " + String(test[i]));
-  }
+    for (int i = 0; i <= stepCount; i++)
+    {
+        Serial.println("Hive weight:");
+        Serial.println(stepCount + ": " + String(hiveWeight[i]) + "kg.");
+    }
 }
 
 void readWeight(int stepCount)
 {
-  Serial.println("reading weight + stepCount " + String(stepCount));
-  test[stepCount] = random(25);
-  Serial.println("Weigt recorded: " + String(test[stepCount]));
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.power_up();
+    delay(10);
+    if (scale.is_ready())
+    {
+        scale.set_scale(LOADCELL_DIVIDER);
+        scale.set_offset(LOADCELL_OFFSET);
+        hiveWeight[stepCount] = int(scale.get_units());
+    }
+    else
+    {
+        /* could not read, possible hardware failure */
+    }
 
-  if (stepCount == 10)
-  {
-    sendData();
-  }
+    if (stepCount == 10)
+    {
+        sendData();
+    }
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  delay(1000); //Take some time to open up the Serial Monitor
-  Serial.println("connection to wifi..");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(10);
-    Serial.print(" ... ");
-  }
-  
+    Serial.begin(115200);
 
-  //Increment boot number and print it every reboot
-  ++bootCount;
-  Serial.println("Boot number: " + String(bootCount));
+    delay(1000); //Take some time to open up the Serial Monitor
 
-  /*
+    //Increment boot number and print it every reboot
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+
+    /*
     First we configure the wake up source
   */
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
-                 " Seconds");
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+                   " Seconds");
 
-  ++stepCount;
-  readWeight(stepCount);
-  if (stepCount == 10)
-  {
-    stepCount = 0;
-  }
+    ++stepCount;
+    readWeight(stepCount);
+    if (stepCount == 10)
+    {
+        stepCount = 0;
+    }
 
-  Serial.println("Going to sleep now");
-  Serial.flush();
-  esp_deep_sleep_start();
-  Serial.println("This will never be printed");
+    Serial.println("Going to sleep now");
+    Serial.flush();
+    esp_deep_sleep_start();
+    Serial.println("This will never be printed");
 }
 
 void loop()
 {
-  //This is not going to be called
+    //This is not going to be called
 }
